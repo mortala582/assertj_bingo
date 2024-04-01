91c6ba0ffba521dd7359d1294dccb8ec68f9396c
Teresa <mortala582@g.educaand.es>
2024-04-01 21:30:56 +0200

    creacion clase test para bingo

---
+++ src/test/org/iesvdm/UtilesArraysTest.java
@@ -0,0 +1,58 @@
+package org.iesvdm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class UtilesArraysTest {
+    @Test
+    void limpiar(){
+
+    }
+    @Test
+    void rellenarACeros(){
+
+    }
+    //sobrecarga de rellenar a ceros
+    @Test
+    void rellenarConValores(){
+
+    }
+    @Test
+    void insertarAlFinal(){
+
+    }
+    @Test
+    void insertarAlPrincipio(){
+
+    }
+    @Test
+    void insertarEnPosicion(){
+
+    }
+    @Test
+    void insertarOrdenado(){
+
+    }
+
+    @Test
+    void eliminarUltimo(){
+
+    }
+    @Test
+    void eliminarPrimero(){
+
+    }
+    @Test
+    void eliminarPosicion(){
+
+    }
+    //elimina un elemento concreto buscandolo por el valor
+    @Test
+    void  eliminar(){
+
+    }
+
+
+}
--- test/org/iesvdm/BingoTest.java
+++
@@ -1,8 +0,0 @@
-package org.iesvdm;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BingoTest {
-
-
-}
