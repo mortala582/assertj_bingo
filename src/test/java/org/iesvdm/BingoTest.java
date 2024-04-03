b347be8038d2d25f27232e8aad323f42e2ec3960
Teresa <mortala582@g.educaand.es>
2024-04-03 20:36:01 +0200

    casi todo el test para bingo

--- src/test/java/org/iesvdm/BingoTest.java
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -3,17 +3,65 @@ package org.iesvdm;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BingoTest {
 
+    /* Aquí, va rellendado las columnas y las filas con los valores corespondientes con números aleatorios.*/
+    /* Voy a validar: 1.- que en cada columna ha insertado el valor de la unidad o decena correspondiente.
+    * 2.- que no se repitan los números en el cartón*/
     @Test
-    void rellenarNumerosCarton(){
+    void rellenarNumerosCartonTest(){
+        int[][] carton = new int[9][3];
+        List<Integer> valoresCarton=new ArrayList<>();
+
+        /* Tengo que rellenarlo con valores y -1 para las comprobaciones*/
+        Bingo.rellenarNumerosCarton(carton);
+        Bingo.ponerBlancos(carton);
+
+        //1.-
+        // Selecciono cada una de las 8 columnas
+        for (int i = 0; i < carton.length; i++){
+            /* Recorro las 3 posiciones de cada columna y voy guardando en un array [] los valores distintos de -1
+            * para luego comprobar el punto2 */
+            for (int j = 0; j < carton[0].length; j++){
+                if (carton[i][j]!=-1){
+                    if(i==0) {
+                        assertThat(carton[0][j]).isBetween(1, 9);
+                    } else if (i==1) {
+                        assertThat(carton[1][j]).isBetween(10,19);
+                    } else if (i==2) {
+                        assertThat(carton[2][j]).isBetween(20,29);
+                    } else if (i==3) {
+                        assertThat(carton[3][j]).isBetween(30, 39);
+                    } else if (i==4) {
+                        assertThat(carton[4][j]).isBetween(40, 49);
+                    } else if (i==5) {
+                        assertThat(carton[5][j]).isBetween(50, 59);
+                    } else if (i==6) {
+                        assertThat(carton[6][j]).isBetween(60, 69);
+                    } else if (i==7) {
+                        assertThat(carton[7][j]).isBetween(70, 79);
+                    } else if (i==8) {
+                        assertThat(carton[8][j]).isBetween(80, 89);
+                    }
+                    valoresCarton.add(carton[i][j]);
+                }
+
+            }
+
+        }
+        //2.-
+        Set<Integer> sinduplicados= new HashSet<>(valoresCarton);
+        assertThat(valoresCarton.size()).isEqualTo(sinduplicados.size());
+
 
     }
     @Test
-    void ponerBlancos(){
+    void ponerBlancosTest(){
         // Genero los arrays columnas:
         int[] col0 = new int[3];
         int[] col1 = new int[3];
@@ -48,6 +96,19 @@ class BingoTest {
 
     }
         assertThat(cont12Blancos).isEqualTo(12);
+
+    }
+    /* buscarFila() y buscarColumna() son parte del método ponerBlancos() que ya hemos testeado,
+     *por lo que no los testeo*/
+
+    /*  buscarValorRepetido() se utiliza tanto para no duplicar los números que saca la bola como para
+    * rellenarNumerosCarton() que ya hemos testeado*/
+
+    /* en pintarCarton() pinta el cartón y realiza esta búsqueda para tachar el número si está en el array de bolas y en el cartón:
+    *boolean seTacha = buscarValorRepetido(arrayBolas, arrayBi[j][i]) volvemos a repetir el método de buscar repetido que ya hemos validado
+    *y pintar el cartón, es bastante gráfico y no se me ocurre mejor manera de testearlo.*/
+    @Test
+    void  insertarAlFinalTest(){
 
     }
     }
