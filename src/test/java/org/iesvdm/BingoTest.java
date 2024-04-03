75a247bcbf87273fb036f8c2c209138eca59507f
mortala <mortala582@g.educaand.es>
2024-04-03 12:21:21 +0200

    solo en rama test

---
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -0,0 +1,73 @@
+package org.iesvdm;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BingoTest {
+
+    @Test
+    void rellenarNumerosCarton(){
+
+    }
+    @Test
+    void ponerBlancos(){
+        // Genero los arrays columnas:
+        int[] col0 = new int[3];
+        int[] col1 = new int[3];
+        int[] col2 = new int[3];
+        int[] col3 = new int[3];
+        int[] col4 = new int[3];
+        int[] col5 = new int[3];
+        int[] col6 = new int[3];
+        int[] col7 = new int[3];
+        int[] col8 = new int[3];
+
+        // Los paso a mi matriz o array bidimensional:
+        int[][] carton = {col0, col1, col2, col3, col4, col5, col6, col7, col8};
+        Bingo.ponerBlancos(carton);
+        for(int j=0;j<carton[0].length;j++){
+            for(int i = 0; i<carton.length; i++){
+                System.out.println((carton[i][j]==0 ? "0" :  carton[i][j])+" ");
+
+            }
+            System.out.println("");
+        }
+        int cont12Blancos=0;
+        for(int[] col: carton){
+
+            int cont=0;
+            cont12Blancos=cont;
+            for (int celda: col){
+                if(celda==-1) cont++;
+                }
+            assertThat(cont).isLessThan(3);
+        }
+        assertThat(cont12Blancos).isEqualTo(12);
+    }
+    @Test
+    void buscarFila(){
+
+    }
+    @Test
+   void buscarColumna(){
+
+   }
+   @Test
+   void buscarValorRepetido(){
+
+   }
+   @Test
+   void pintarCarton(){
+
+   }
+   @Test
+   void insertarAlFinal(){
+
+   }
+   void ordenar(){
+
+   }
+}
